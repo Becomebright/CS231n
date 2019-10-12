a539b6b6b96fc97c6ad7c5e8d250c22f5be3a88b
apple <dsz6223042@buaa.edu.cn>
2019-10-13 00:15:56 +0800

    Start 3-1 RNN

---
+++ configuration.sh
@@ -0,0 +1,6 @@
+pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
+
+pip install jupyter_contrib_nbextensions
+jupyter contrib nbextension install --user
+
+pip install future
